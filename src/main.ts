112713084008c7703c78975b937adc859b2ae78d
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-11-26 22:45:00 +0100

    added zooming in and mouse dragging

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,29 @@ import './style.css'
 const canvas = <HTMLCanvasElement>document.getElementById('myCanvas')
 const context = canvas.getContext('2d')
 
-const maxIterations = 1000
+const maxIterations = 100
 const width = canvas.width
 const height = canvas.height
+let scale = 4
+let offsetX = 0
+let offsetY = 0
+
+// Variables for mouse dragging
+var isDragging = false
+var dragStartX = 0
+var dragStartY = 0
 
 function plotMandelbrot() {
   for (let Px = 0; Px < width; Px++) {
     for (let Py = 0; Py < height; Py++) {
-      let x0 = ((Px - width / 2) * 4) / width
-      let y0 = ((Py - height / 2) * 4) / height
+      let x0 = ((Px - width / 2 + offsetX) * scale) / width
+      let y0 = ((Py - height / 2 + offsetY) * scale) / height
 
       let x = 0
       let y = 0
       let iteration = 0
 
+      //calculate whether complex number x0 + y0 is in Mandelbrod set or not
       while (x * x + y * y <= 2 * 2 && iteration < maxIterations) {
         let xTemp = x * x - y * y + x0
         y = 2 * x * y + y0
@@ -34,5 +43,54 @@ function plotMandelbrot() {
     }
   }
 }
+
+function handleWheel(event: any) {
+  var delta = event.deltaY || event.detail || event.wheelDelta
+  var zoomFactor = delta < 0 ? 0.9 : 1.1
+
+  // Calculate the position of the mouse in the fractal coordinates
+  var mouseX = event.clientX - canvas.getBoundingClientRect().left
+  var mouseY = event.clientY - canvas.getBoundingClientRect().top
+
+  // Update offsets based on the mouse position and zoom factor
+  offsetX += (mouseX - canvas.width / 2 + offsetX) * (1 - zoomFactor)
+  offsetY += (mouseY - canvas.height / 2 + offsetY) * (1 - zoomFactor)
+
+  // Adjust the scale based on the zoom factor
+  scale *= zoomFactor
+
+  plotMandelbrot()
+}
+
+function handleMouseDown(event: any) {
+  isDragging = true
+  dragStartX = event.clientX
+  dragStartY = event.clientY
+}
+
+function handleMouseMove(event: any) {
+  if (isDragging) {
+    var deltaX = event.clientX - dragStartX
+    var deltaY = event.clientY - dragStartY
+
+    offsetX -= deltaX
+    offsetY -= deltaY
+
+    dragStartX = event.clientX
+    dragStartY = event.clientY
+
+    plotMandelbrot()
+  }
+}
+
+function handleMouseUp() {
+  isDragging = false
+}
+
+// Add event listeners for mouse interaction
+canvas.addEventListener('wheel', handleWheel)
+canvas.addEventListener('mousedown', handleMouseDown)
+canvas.addEventListener('mousemove', handleMouseMove)
+canvas.addEventListener('mouseup', handleMouseUp)
 
 plotMandelbrot()
