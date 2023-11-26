d0dd3f44d1e6b76c7cf041ddfb9a50e49d30b078
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-11-26 23:34:12 +0100

    added smooth coloring

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 const canvas = <HTMLCanvasElement>document.getElementById('myCanvas')
 const context = canvas.getContext('2d')
 
-const maxIterations = 1000
+const maxIterations = 100
 const width = canvas.width
 const height = canvas.height
 let scale = 3
@@ -51,18 +51,52 @@ function plotMandelbrot() {
         x = xTemp
         iteration++
       }
-
+  
       */
 
-      //if reached max iterations then color is 0 else other color is chosen
-      let color = iteration === maxIterations ? 0 : iteration % 265
+      // Use continuous coloring
+      var smoothColor = iteration + 1 - Math.log(Math.log(Math.sqrt(x * x + y * y))) / Math.log(2)
+
+      // Map the smooth color to the HSL color space
+      var hue = (smoothColor % 256) / 256
+      var saturation = 1
+      var lightness = iteration === maxIterations ? 0 : 0.5
+
+      // Convert HSL to RGB
+      var rgb = hslToRgb(hue, saturation, lightness)
 
       if (context) {
-        context.fillStyle = 'rgb(' + color + ',' + color + ',' + color + ')'
+        context.fillStyle = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'
         context.fillRect(Px, Py, 1, 1)
       }
     }
   }
+}
+
+function hslToRgb(h: number, s: number, l: number) {
+  var r, g, b
+
+  if (s === 0) {
+    r = g = b = l // achromatic
+  } else {
+    var hue2rgb = function hue2rgb(p: number, q: number, t: number) {
+      if (t < 0) t += 1
+      if (t > 1) t -= 1
+      if (t < 1 / 6) return p + (q - p) * 6 * t
+      if (t < 1 / 2) return q
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+      return p
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s
+    var p = 2 * l - q
+
+    r = hue2rgb(p, q, h + 1 / 3)
+    g = hue2rgb(p, q, h)
+    b = hue2rgb(p, q, h - 1 / 3)
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)]
 }
 
 function handleWheel(event: any) {
